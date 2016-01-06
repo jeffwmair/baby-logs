a3728e48a2d608b6d942b51afd3c98c20b8ec3e7
Jeff Mair <jeffwmair@gmail.com>
2016-01-05 20:51:13 -0500

    updated queries to include only records up until now

--- src/mapping/RecordQueryMapper.php
+++ src/mapping/RecordQueryMapper.php
@@ -26,11 +26,10 @@ class RecordQueryMapper {
 
 	public function getAllDays() {
 
-		$days = array();
-
-		$sql = "select id, start, end, DATE_FORMAT(start, '%Y-%m-%d') as day from baby_sleep order by start ASC";
+		$sql = "select id, start, end, DATE_FORMAT(start, '%Y-%m-%d') as day from baby_sleep where start <= CURRENT_TIMESTAMP() order by start ASC";
 		$sleepRows  = getSqlResult($sql);
 
+		$days = array();
 		while ($row = @ mysql_fetch_array($sleepRows, MYSQL_ASSOC))  {
 			$day = $this->createDayIfNotExists($days, $row['day']);
 			$sleepRecord = new SleepRecord(new DateTime($row['start']), new DateTime($row['end']));
@@ -49,7 +48,7 @@ class RecordQueryMapper {
 			}
 		}
 
-		$sql = "select time, DATE_FORMAT(time, '%Y-%m-%d') as day, entry_type, entry_value from baby_keyval order by time ASC";
+		$sql = "select time, DATE_FORMAT(time, '%Y-%m-%d') as day, entry_type, entry_value from baby_keyval WHERE time <= CURRENT_TIMESTAMP() order by time ASC";
 		$rows  = getSqlResult($sql);
 
 		while ($row = @ mysql_fetch_array($rows, MYSQL_ASSOC))  {
@@ -69,7 +68,7 @@ class RecordQueryMapper {
 	}
 
 	public function getLatestDiaperRecord($diapertype) {
-		$sql = "select time, entry_value from baby_keyval where entry_type = 'diaper' and entry_value = '$diapertype' order by time DESC limit 1";
+		$sql = "select time, entry_value from baby_keyval where entry_type = 'diaper' and entry_value = '$diapertype' and time <= CURRENT_TIMESTAMP() order by time DESC limit 1";
 		$rows  = getSqlResult($sql);
 		$row = @ mysql_fetch_array($rows, MYSQL_ASSOC);
 		$record = new DiaperRecord( $row['time'], $row['entry_value'] );
@@ -78,7 +77,7 @@ class RecordQueryMapper {
 
 
 	public function getLatestSleepRecord() {
-		$sql = "select id, start, end from baby_sleep order by start DESC limit 1";
+		$sql = "select id, start, end from baby_sleep where start <= CURRENT_TIMESTAMP() order by start DESC limit 1";
 		$rows  = getSqlResult($sql);
 		$row = @ mysql_fetch_array($rows, MYSQL_ASSOC);
 		$record = new SleepRecord( new DateTime( $row['start'] ), new DateTime( $row['end'] ));
@@ -86,7 +85,7 @@ class RecordQueryMapper {
 	}
 
 	public function getLatestFeedRecord() {
-		$sql = "select time, entry_value from baby_keyval where entry_type = 'feed' order by time DESC limit 1";
+		$sql = "select time, entry_value from baby_keyval where entry_type = 'feed' and time <= CURRENT_TIMESTAMP() order by time DESC limit 1";
 		$rows  = getSqlResult($sql);
 		$row = @ mysql_fetch_array($rows, MYSQL_ASSOC);
 		$record = new FeedRecord( new DateTime( $row['time'] ), $row['entry_value'] );
