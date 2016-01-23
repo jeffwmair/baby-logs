048b3a793e6dac610b17237beaf2941e999caf09
Jeff Mair <jeffwmair@gmail.com>
2016-01-22 21:15:49 -0500

    removed authentication and admin pages for now...

--- signout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// clear the cookie by setting its expiration time in the past
-	setcookie('babyloggersession', '', time()-3600, '/');
-
-	// bounce them back to the login page
-	header('Location: '.$urlprefix.'signin.php');
-?>
