a79a314d414231b4485e377b4dcd1838d42f5cb5
Jeff Mair <jeffwmair@gmail.com>
2016-03-09 21:55:29 -0500

    added some helper scripts

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -7,18 +7,18 @@ fi
 SERVER=$1
 
 ssh $SERVER << _EOF_
-mkdir -p /var/www/html/ljpy
+mkdir -p /var/www/html/liamjournal
 _EOF_
 
 rsync -azP * \
 	--exclude "*.swp" \
-	--exclude "*.sh" \
+	--exclude "notes" \
 	--exclude ".DS_Store" \
 	--exclude ".gitignore" \
-	--exclude "README.md" \
-	--exclude "credentials.properties" \
+	--exclude "README*md" \
+	--exclude "credentials.properties*" \
 	--exclude "scripts/" \
 	--exclude "database/" \
 	--exclude "test/" \
 	--exclude "venv/" \
-	$SERVER:/var/www/html/ljpy
+	$SERVER:/var/www/html/liamjournal
