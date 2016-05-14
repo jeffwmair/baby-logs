8c327f20d25ddeb3e2d30ad78deb36b948a4527c
Jeff Mair <jeffwmair@gmail.com>
2016-05-13 21:48:09 -0400

    removed misc query_mapper_test

--- query_mapper_test.py
+++
@@ -1,8 +0,0 @@
-from app.properties_reader import PropertiesReader
-from app.query_mapper import QueryMapper
-
-reader = PropertiesReader('credentials.properties')
-creds = reader.read_from_file()
-babyid = 1
-mapper = QueryMapper(creds, babyid)
-mapper.get_chart_data_daily(10)
