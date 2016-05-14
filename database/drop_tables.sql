4be35e786b82eb381fce732795adde700b523413
Jeff Mair <jeffwmair@gmail.com>
2016-05-14 16:21:30 -0400

    Improved drop-tables script

--- database/drop_tables.sql
+++ database/drop_tables.sql
@@ -1,5 +1,6 @@
-drop table baby_log;
-drop table baby_sleep;
-drop table baby_keyval;
-drop table guardian;
-drop table baby;
+drop table if exists baby_log;
+drop table if exists baby_sleep;
+drop table if exists baby_keyval;
+drop table if exists guardian;
+drop table if exists usersession;
+drop table if exists baby;
