4277d9a2ff9c8adb87c05ee31b28bba60d477aa9
Jeff Mair <jeffwmair@gmail.com>
2016-05-15 15:43:09 -0400

    script comment cleanup

--- activate_environment.sh
+++ activate_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# NOTE: you should source this script.  Eg: $ source activate_environment.sh (or . activate_environment.sh), as well other scripts need to be sourced.
+# NOTE: you should source this script.  Eg: $ source activate_environment.sh (or . activate_environment.sh)
 
 export BABY_LOGGER=`pwd`
 . venv/bin/activate
