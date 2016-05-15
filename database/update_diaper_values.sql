4923b6cfdb548af94a0cd2205c83913a55503e40
Jeff Mair <jeffwmair@gmail.com>
2016-05-14 22:39:28 -0400

    removed old upgrade scripts

--- database/update_diaper_values.sql
+++
@@ -1,3 +0,0 @@
-update baby_keyval set babyid = babyid, time = time, entry_type = entry_type, entry_value = 'pee' where entry_type = 'diaper' and entry_value = '1';
-update baby_keyval set babyid = babyid, time = time, entry_type = entry_type, entry_value = 'poo' where entry_type = 'diaper' and entry_value = '2';
-update baby_keyval set babyid = babyid, time = time, entry_type = entry_type, entry_value = 'poo' where entry_type = 'diaper' and entry_value = '3';
--- database/usersession.sql
+++
@@ -1,8 +0,0 @@
-drop table usersession;
-create table usersession (
-	token varchar(250) NOT NULL,
-	babyid INT(6) NOT NULL,
-	expiration TIMESTAMP NOT NULL,
-	constraint usersession_uc UNIQUE (token),
-	constraint usersession_baby FOREIGN KEY (babyid) REFERENCES baby (id)
-);
