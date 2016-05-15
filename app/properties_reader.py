492f9419470c3b4c345b060b30b59d1a02fa775d
Jeff Mair <jeffwmair@gmail.com>
2016-05-14 23:11:51 -0400

    Added code to allow for #-prefixed comments in the credentials file, made credentials sample more obvious

--- app/properties_reader.py
+++ app/properties_reader.py
@@ -8,6 +8,8 @@ class PropertiesReader:
 		f = open(self._filename, 'r')
 
 		for line in f:
+			if line.startswith('#'):
+				continue
 			keyval = line.replace('\n', '').split('=')
 			props[keyval[0]] = keyval[1]
 
