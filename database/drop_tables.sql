bcbe6c062aad0a596c52065f956292d9329dfdf7
Jeff Mair <jeffwmair@gmail.com>
2016-05-15 15:41:46 -0400

    cleanup of drop tables which had some old no-longer existing tables

--- database/drop_tables.sql
+++ database/drop_tables.sql
@@ -1,6 +1,3 @@
 drop table if exists baby_log;
 drop table if exists baby_sleep;
 drop table if exists baby_keyval;
-drop table if exists guardian;
-drop table if exists usersession;
-drop table if exists baby;
