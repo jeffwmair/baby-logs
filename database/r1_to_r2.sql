130d7b2f88864bb5394bb2a7d9d69e4a0f02848a
Jeff Mair <jeffwmair@gmail.com>
2017-04-04 22:46:39 -0400

    Added sql script to create json_objects table

---
+++ database/r1_to_r2.sql
@@ -0,0 +1,6 @@
+create table json_objects (
+	json_key varchar(200) PRIMARY KEY,
+	babyid INT(4) NOT NULL,
+	json_content text NOT NULL,
+	constraint const_uk_json_objects UNIQUE (json_key, babyid)
+);
