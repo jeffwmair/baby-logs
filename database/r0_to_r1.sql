45fd0fca5e64d55ea07ad96de77e059528f9a374
Jeff Mair <jeffwmair@gmail.com>
2018-06-04 22:04:53 -0400

    changed tabs to spaces

--- database/r0_to_r1.sql
+++ database/r0_to_r1.sql
@@ -1,23 +1,23 @@
 create table baby_sleep (
-	id INT(6) AUTO_INCREMENT PRIMARY KEY,
-	babyid INT(6),
-	start TIMESTAMP NULL,
-	end TIMESTAMP NULL,
-	constraint const_uk UNIQUE (start)
+    id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    babyid INT(6),
+    start TIMESTAMP NULL,
+    end TIMESTAMP NULL,
+    constraint const_uk UNIQUE (start)
 );
 
 create table baby_keyval(
-	id INT(6) AUTO_INCREMENT PRIMARY KEY,
-	babyid INT(6),
-	time TIMESTAMP NOT NULL,
-	entry_type varchar(10) NOT NULL,
-	entry_value varchar(8) NOT NULL,
-	constraint const_uk_babykeyval UNIQUE (time, entry_type, entry_value)
+    id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    babyid INT(6),
+    time TIMESTAMP NOT NULL,
+    entry_type varchar(10) NOT NULL,
+    entry_value varchar(8) NOT NULL,
+    constraint const_uk_babykeyval UNIQUE (time, entry_type, entry_value)
 );
 
 create table baby_log(
-	id INT(6) AUTO_INCREMENT PRIMARY KEY,
-	time TIMESTAMP NOT NULL,
-	log_type varchar(10) NOT NULL,
-	message varchar(1000) NOT NULL
+    id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    time TIMESTAMP NOT NULL,
+    log_type varchar(10) NOT NULL,
+    message varchar(1000) NOT NULL
 );
