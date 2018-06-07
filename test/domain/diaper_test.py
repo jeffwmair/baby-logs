95f4f802fcf3f86e1bcfcd87aea0a59e204c37cb
Jeff Mair <jeffwmair@gmail.com>
2018-06-06 22:21:29 -0400

    changed tabs to spaces

--- test/domain/diaper_test.py
+++ test/domain/diaper_test.py
@@ -5,23 +5,23 @@ from app.db_records import KeyValueRecord
 
 class DiaperTest(unittest.TestCase):
 
- 	def test_poo_count(self):
- 		p1 = KeyValueRecord(datetime(2000, 1, 1, 0, 0), 'diaper', 'poo', 1)
- 		p2 = KeyValueRecord(datetime(2001, 1, 1, 0, 15), 'diaper', 'poo', 1)
- 		diapers = [p1, p2]
- 		sut = DiaperSet(diapers)
+     def test_poo_count(self):
+         p1 = KeyValueRecord(datetime(2000, 1, 1, 0, 0), 'diaper', 'poo', 1)
+         p2 = KeyValueRecord(datetime(2001, 1, 1, 0, 15), 'diaper', 'poo', 1)
+         diapers = [p1, p2]
+         sut = DiaperSet(diapers)
  
- 		self.assertEqual(0, sut.get_pee_count())
- 		self.assertEqual(2, sut.get_poo_count())
+         self.assertEqual(0, sut.get_pee_count())
+         self.assertEqual(2, sut.get_poo_count())
  
-	def test_pee_count(self):
-		p1 = KeyValueRecord(datetime(2000, 1, 1, 0, 0), 'diaper', 'pee', 1)
-		p2 = KeyValueRecord(datetime(2000, 1, 1, 0, 15), 'diaper', 'pee', 1)
-		diapers = [p1, p2]
-		sut = DiaperSet(diapers)
+    def test_pee_count(self):
+        p1 = KeyValueRecord(datetime(2000, 1, 1, 0, 0), 'diaper', 'pee', 1)
+        p2 = KeyValueRecord(datetime(2000, 1, 1, 0, 15), 'diaper', 'pee', 1)
+        diapers = [p1, p2]
+        sut = DiaperSet(diapers)
 
-		self.assertEqual(2, sut.get_pee_count())
-		self.assertEqual(0, sut.get_poo_count())
+        self.assertEqual(2, sut.get_pee_count())
+        self.assertEqual(0, sut.get_poo_count())
 
 if __name__ == '__main__':
     unittest.main()
--- test/domain/sleep_test.py
+++ test/domain/sleep_test.py
@@ -5,64 +5,64 @@ from app.db_records import SleepRecord
 
 class SleepTest(unittest.TestCase):
 
-	# merge 4 records down to 2
-	def test_merge_contig_sleeps(self):
+    # merge 4 records down to 2
+    def test_merge_contig_sleeps(self):
 
-		rec1 = SleepRecord(datetime(2016, 2, 1, 0, 0), datetime(2016, 2, 1, 0, 15))
-		rec2 = SleepRecord(datetime(2016, 2, 1, 0, 15), datetime(2016, 2, 1, 0, 30))
-		rec3 = SleepRecord(datetime(2016, 2, 1, 8, 0), datetime(2016, 2, 1, 8, 15))
-		rec4 = SleepRecord(datetime(2016, 2, 1, 8, 15), datetime(2016, 2, 1, 9, 0))
-		records = [ rec1, rec2, rec3, rec4 ]
+        rec1 = SleepRecord(datetime(2016, 2, 1, 0, 0), datetime(2016, 2, 1, 0, 15))
+        rec2 = SleepRecord(datetime(2016, 2, 1, 0, 15), datetime(2016, 2, 1, 0, 30))
+        rec3 = SleepRecord(datetime(2016, 2, 1, 8, 0), datetime(2016, 2, 1, 8, 15))
+        rec4 = SleepRecord(datetime(2016, 2, 1, 8, 15), datetime(2016, 2, 1, 9, 0))
+        records = [ rec1, rec2, rec3, rec4 ]
 
-		sut = SleepSet(records)
-		recs = sut.get_merged_records()
-		self.assertEqual(2, len(recs), 'should be 2 records now')
-		self.assertEqual(datetime(2016,2,1,0,0), recs[0].start)
-		self.assertEqual(datetime(2016,2,1,0,30), recs[0].end)
-		self.assertEqual(datetime(2016,2,1,8,0), recs[1].start)
-		self.assertEqual(datetime(2016,2,1,9,0), recs[1].end)
+        sut = SleepSet(records)
+        recs = sut.get_merged_records()
+        self.assertEqual(2, len(recs), 'should be 2 records now')
+        self.assertEqual(datetime(2016,2,1,0,0), recs[0].start)
+        self.assertEqual(datetime(2016,2,1,0,30), recs[0].end)
+        self.assertEqual(datetime(2016,2,1,8,0), recs[1].start)
+        self.assertEqual(datetime(2016,2,1,9,0), recs[1].end)
 
-	def test_daytime_records(self):
-		rec1 = SleepRecord(datetime(2016, 1, 1, 7, 0), datetime(2016, 1, 1, 9, 0))
-		records = [ rec1 ]
-		sut = SleepSet(records)
-		sleeps_day = sut.get_nap_hrs()
+    def test_daytime_records(self):
+        rec1 = SleepRecord(datetime(2016, 1, 1, 7, 0), datetime(2016, 1, 1, 9, 0))
+        records = [ rec1 ]
+        sut = SleepSet(records)
+        sleeps_day = sut.get_nap_hrs()
 
-		self.assertEqual(2, sleeps_day)
+        self.assertEqual(2, sleeps_day)
 
-	def test_lastnight_records(self):
-		rec1 = SleepRecord(datetime(2016, 1, 1, 0, 0), datetime(2016, 1, 1, 0, 15))
-		records = [ rec1 ]
-		sut = SleepSet(records)
-		sleeps_day = sut.get_nap_hrs()
+    def test_lastnight_records(self):
+        rec1 = SleepRecord(datetime(2016, 1, 1, 0, 0), datetime(2016, 1, 1, 0, 15))
+        records = [ rec1 ]
+        sut = SleepSet(records)
+        sleeps_day = sut.get_nap_hrs()
 
-		self.assertEqual(0, sleeps_day)
+        self.assertEqual(0, sleeps_day)
 
-	def test_unbroken_sleep(self):
-		# Sleep from 8pm till 4am then 5am to 7am.
-		# This should yield an 8 hour unbroken stretch
+    def test_unbroken_sleep(self):
+        # Sleep from 8pm till 4am then 5am to 7am.
+        # This should yield an 8 hour unbroken stretch
 
-		# this "sleep set" is for Jan 1st.  The overnight records are from Jan 2nd, not 
-		# part of this day, so the records need to be added outside the constructor.
+        # this "sleep set" is for Jan 1st.  The overnight records are from Jan 2nd, not 
+        # part of this day, so the records need to be added outside the constructor.
 
-		rec1 = SleepRecord(datetime(2016, 1, 1, 20, 0, 0), datetime(2016, 1, 2, 4, 0, 0))
-		records = [ rec1 ]
-		sut = SleepSet(records)
+        rec1 = SleepRecord(datetime(2016, 1, 1, 20, 0, 0), datetime(2016, 1, 2, 4, 0, 0))
+        records = [ rec1 ]
+        sut = SleepSet(records)
 
-		rec2 = SleepRecord(datetime(2016, 1, 2, 5, 0, 0), datetime(2016, 1, 2, 7, 0, 0))
-		records_overnight = [ rec2 ]
-		sut.set_overnight_sleep_records(records_overnight)
+        rec2 = SleepRecord(datetime(2016, 1, 2, 5, 0, 0), datetime(2016, 1, 2, 7, 0, 0))
+        records_overnight = [ rec2 ]
+        sut.set_overnight_sleep_records(records_overnight)
 
-		self.assertEqual(8.0, sut.get_unbroken_night_sleep_hrs())
+        self.assertEqual(8.0, sut.get_unbroken_night_sleep_hrs())
 
-	def test_naps(self):
-		rec1 = SleepRecord(datetime(2016, 1, 1, 9, 0), datetime(2016, 1, 1, 9, 15))
-		rec2 = SleepRecord(datetime(2016, 1, 1, 9, 15), datetime(2016, 1, 1, 9, 30))
-		rec3 = SleepRecord(datetime(2016, 1, 1, 12, 0), datetime(2016, 1, 1, 12, 15))
-		rec4 = SleepRecord(datetime(2016, 1, 1, 12, 15), datetime(2016, 1, 1, 12, 30))
+    def test_naps(self):
+        rec1 = SleepRecord(datetime(2016, 1, 1, 9, 0), datetime(2016, 1, 1, 9, 15))
+        rec2 = SleepRecord(datetime(2016, 1, 1, 9, 15), datetime(2016, 1, 1, 9, 30))
+        rec3 = SleepRecord(datetime(2016, 1, 1, 12, 0), datetime(2016, 1, 1, 12, 15))
+        rec4 = SleepRecord(datetime(2016, 1, 1, 12, 15), datetime(2016, 1, 1, 12, 30))
 
-		records = [ rec1, rec2, rec3, rec4 ]
-		sut = SleepSet(records)
+        records = [ rec1, rec2, rec3, rec4 ]
+        sut = SleepSet(records)
 
-		self.assertEqual(2, sut.get_nap_count())
-		self.assertEqual(1.0, sut.get_nap_hrs())
+        self.assertEqual(2, sut.get_nap_count())
+        self.assertEqual(1.0, sut.get_nap_hrs())
