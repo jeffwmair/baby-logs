6b2c697f37aee6d3b314e8b5595331fd9a926c30
Jeff Mair <jeffwmair@gmail.com>
2018-06-14 23:13:36 -0400

    tabs -> spaces

--- test/domain/test_diaper.py
+++ test/domain/test_diaper.py
@@ -10,10 +10,10 @@ class DiaperTest(unittest.TestCase):
          p2 = KeyValueRecord(datetime(2001, 1, 1, 0, 15), 'diaper', 'poo', 1)
          diapers = [p1, p2]
          sut = DiaperSet(diapers)
- 
+
          self.assertEqual(0, sut.get_pee_count())
          self.assertEqual(2, sut.get_poo_count())
- 
+
     def test_pee_count(self):
         p1 = KeyValueRecord(datetime(2000, 1, 1, 0, 0), 'diaper', 'pee', 1)
         p2 = KeyValueRecord(datetime(2000, 1, 1, 0, 15), 'diaper', 'pee', 1)
